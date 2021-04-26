f7ffef06050bb1536b5144f94b34d616fa395ce8
jafallon <45009373+jafallon@users.noreply.github.com>
2021-04-26 10:08:31 -0500

    Test #3

--- pyspark/bronze_silver.py
+++ pyspark/bronze_silver.py
@@ -7,6 +7,7 @@ from pyspark.sql import functions as psf
 import os
 ####test
 #Test2
+# Test3
 
 spark = SparkSession.builder.appName("DataOps").config("hive.metastore.connect.retries",5).config("hive.metastore.client.factory.class","com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory").enableHiveSupport().getOrCreate()
 sqlContext = SQLContext(spark)
